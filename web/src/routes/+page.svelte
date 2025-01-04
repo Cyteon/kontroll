665a189bf423930ecdac23be36a5012969f86625
cyteon <cyteon@hackclub.app>
2025-01-04 22:01:37 +0100

    website and name change

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import DarkLightMode from "@/components/other/DarkLightMode.svelte";
+</script>
+
+<div class="flex h-screen w-full">
+    <div class="text-center w-full">
+        <h1 class="text-8xl mt-8 font-bold">Kontrl</h1>
+        <p class="text-xl mx-auto">Making user managment great again</p>
+    </div>
+
+    <div class="absolute bottom-3 left-3">
+        <DarkLightMode />
+    </div>
+</div>
